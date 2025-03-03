7e5cca645de52db11d307a7617c5718a4c44a642
Geovanny-Quintero-Velez <geovannyalexander07@gmail.com>
2025-03-03 15:51:15 -0500

    Document endpoints using swagger

--- src/main/java/co/analisys/biblioteca/controller/UsuarioController.java
+++ src/main/java/co/analisys/biblioteca/controller/UsuarioController.java
@@ -4,6 +4,8 @@ import co.analisys.biblioteca.model.Email;
 import co.analisys.biblioteca.model.Usuario;
 import co.analisys.biblioteca.model.UsuarioId;
 import co.analisys.biblioteca.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,11 +15,21 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @Operation(
+        summary = "Obtener usuario por ID",
+        description = "Este endpoint permite obtener la información de un usuario a partir de su ID. " +
+                      "El ID del usuario es proporcionado como parte de la URL."
+    )
     @GetMapping("/{id}")
     public Usuario obtenerUsuario(@PathVariable String id) {
         return usuarioService.obtenerUsuario(new UsuarioId(id));
     }
 
+    @Operation(
+        summary = "Cambiar correo electrónico de usuario",
+        description = "Este endpoint permite cambiar la dirección de correo electrónico de un usuario. " +
+                      "El nuevo correo electrónico se pasa como un objeto JSON en el cuerpo de la solicitud."
+    )
     @PutMapping("/{id}/email")
     public void cambiarEmail(@PathVariable String id, @RequestBody String nuevoEmail) {
         usuarioService.cambiarEmailUsuario(new UsuarioId(id), new Email(nuevoEmail));
